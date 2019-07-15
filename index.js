c74a72dcdfb3b9a0ba1ea2e40cc635d834e2012e
Mark Edosa <maqui7295@gmail.com>
2019-07-15 17:27:29 +0100

    setup postgres database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+
 const authRouter = require('./routes/auth');
 const tripRouter = require('./routes/trips');
+
 
 const apiVersion = '/api/v1/';
 
